34a51ad7bd25c91531100d2ca9d0548dcd668fdd
nero <swellchaser33@gmail.com>
2019-03-29 17:37:41 +0100

    fix github url

--- EzWebScraping/example.py
+++ EzWebScraping/example.py
@@ -28,7 +28,7 @@ def main():
 
     scraper = EzWebScraping()
     # The url corresponds to the content of "action" in the <form> tag.
-    scraper.connect('http://github.co/session',
+    scraper.connect('http://github.com/session',
                     payload=payload,
                     auth_token_name="authenticity_token")
 
